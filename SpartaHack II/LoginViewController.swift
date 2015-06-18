306b7b7c8f89eab4ba2f035120f036dd2365538f
cnmcgrath <chrisnmcgrath@gmail.com>
2015-06-17 23:21:40 -0500

    added login view and parse frameworks

---
+++ SpartaHack II/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  SpartaHack II
+//
+//  Created by Chris McGrath on 6/17/15.
+//  Copyright (c) 2015 Chris McGrath. All rights reserved.
+//
+
+import Foundation
