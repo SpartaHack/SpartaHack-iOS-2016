3cb724aac84df2407312b213a8cc2ba5d9a0ba6a
cnmcgrath <chrisnmcgrath@gmail.com>
2015-06-20 23:09:27 -0500

    added login and signup views, will validate with parse soon

---
+++ SpartaHack II/SignupViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SignupViewController.swift
+//  SpartaHack II
+//
+//  Created by Chris McGrath on 6/20/15.
+//  Copyright (c) 2015 Chris McGrath. All rights reserved.
+//
+
+import Foundation
