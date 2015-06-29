867c524fea84946b61a943e2f046284163ac5f89
cnmcgrath <chrisnmcgrath@gmail.com>
2015-06-28 23:05:23 -0500

    added signin funcintality and put placeholder for profile view

---
+++ SpartaHack II/ParseUserModel.swift
@@ -0,0 +1,13 @@
+//
+//  ParseUserModel.swift
+//  SpartaHack II
+//
+//  Created by Chris McGrath on 6/28/15.
+//  Copyright (c) 2015 Chris McGrath. All rights reserved.
+//
+
+import Cocoa
+
+class ParseUserModel: NSObject {
+
+}
