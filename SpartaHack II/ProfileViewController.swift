e3015accf052ec6d28d49c3842ef4eb822949dd9
cnmcgrath <chrisnmcgrath@gmail.com>
2015-06-28 23:38:29 -0500

    added frames for more views

---
+++ SpartaHack II/ProfileViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ProfileViewController.swift
+//  SpartaHack II
+//
+//  Created by Chris McGrath on 6/28/15.
+//  Copyright (c) 2015 Chris McGrath. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ProfileViewController: UIViewController {
+
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    var user = PFUser.currentUser()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        userNameLabel.text = user?.username
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
