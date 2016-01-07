2b0f1b4b3349d739f8e2821895fcd677c1249c74
cnmcgrath <chrisnmcgrath@gmail.com>
2016-01-07 16:28:31 -0500

    fixed profile view weirdness when logging in for the first time

---
+++ SpartaHack II/RootTabBarViewController.swift
@@ -0,0 +1,45 @@
+//
+//  RootTabBarViewController.swift
+//  SpartaHack II
+//
+//  Created by Chris McGrath on 1/7/16.
+//  Copyright © 2016 Chris McGrath. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class RootTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func profileButtonTapped(sender: AnyObject) {
+        if PFUser.currentUser() == nil {
+            // load the login view
+            self.navigationController?.performSegueWithIdentifier("loginSegue", sender: nil)
+        } else {
+            // load the profile
+            self.navigationController?.performSegueWithIdentifier("profileSegue", sender: nil)
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
