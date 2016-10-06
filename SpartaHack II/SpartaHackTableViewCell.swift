1c61c0afb6aac0d513f296e17d90d89df65dd7b4
cnmcgrath <chrisnmcgrath@gmail.com>
2016-10-06 14:08:19 -0400

    Added blank .xib files to project and blank override classes

---
+++ SpartaHack II/SpartaHackTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SpartaHackTableViewCell.swift
+//  SpartaHack 2016
+//
+//  Created by Chris McGrath on 10/6/16.
+//  Copyright © 2016 Chris McGrath. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// Implements an Extension to UITableViewCell
+class SpartahackTableViewCell : UITableViewCell {
+    
+}
---
+++ SpartaHack II/SpartaHackView.swift
@@ -0,0 +1,16 @@
+//
+//  SpartaHackView.swift
+//  SpartaHack 2016
+//
+//  Created by Chris McGrath on 10/6/16.
+//  Copyright © 2016 Chris McGrath. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// implements a uiview subclass so colors and stuff all match.
+// Everyview should be a SpartahackView
+class SpartahackView: UIView {
+    
+}
