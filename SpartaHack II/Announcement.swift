11edb4803c28a1b8ae8807818fd1491a6ec7ab12
cnmcgrath <chrisnmcgrath@gmail.com>
2016-10-16 16:42:58 -0400

    fixed my mistakes

---
+++ SpartaHack II/Announcement.swift
@@ -0,0 +1,39 @@
+//
+//  Announcement.swift
+//  SpartaHack 2016
+//
+//  Created by Chris McGrath on 9/9/16.
+//  Copyright © 2016 Chris McGrath. All rights reserved.
+//
+
+import Foundation
+
+
+/* Announcement Object
+* Stores all details about announcements
+*/
+
+class Announcement: NSObject {
+    var id: String! = nil
+    var title: String! = nil
+    var detail: String!
+    var pinned: Bool! = false
+    var createdTime: NSDate! = nil
+    var updatedTime: NSDate? = nil
+    
+    override init() {
+        super.init()
+    }
+    
+    override var debugDescription : String {
+        let announcement =
+            "id: \(id!)" +
+            "\n title:\(title!)" +
+            "\n detail:\(detail!)" +
+            "\n pinned:\(pinned!)" +
+            "\n created:\(createdTime!)" +
+            "\n updated:\(updatedTime)"
+            
+        return announcement
+    }
+}
