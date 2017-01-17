4daf5c48ac45fe377bf64938893a4728bd13b210
cnmcgrath <chrisnmcgrath@gmail.com>
2017-01-16 22:26:31 -0500

    sponsor fix

--- SpartaHack II/SpartaModel.swift
+++ SpartaHack II/SpartaModel.swift
@@ -170,51 +170,49 @@ class SpartaModel: NSObject {
             // get our schedule data
             
             if let result = response.result.value {
-                if let json = result as? NSDictionary {
-                    if let objArray = json["schedule"] as? [NSDictionary] {
-                        // loop through our valid json dictionary and create event objects that will be added to the schedule
-                        for obj in objArray {
-                            // create event objects
-                            let event = Event()
-                            
-                            guard let id = obj["id"] as? Int else {
-                                fatalError("ToDo: gracefully handle error")
-                            }
-                            event.id = id
-                            
-                            guard let title = obj["title"] as? String else {
-                                fatalError("ToDo: gracefully handle error")
-                            }
-                            event.title = title
-                            
-                            guard let detail = obj["description"] as? String else {
-                                fatalError("ToDo: gracefully handle error")
-                            }
-                            event.detail = detail
-                            
-                            guard let timeStr = obj["time"] as? String,
-                                let time = self.formatter.date(from: timeStr) as NSDate? else {
-                                fatalError("ToDo: gracefully handle error")
-                            }
-                            event.time = time
-                            
-                            guard let location = obj["location"] as? String else {
-                                fatalError("ToDo: gracefully handle error")
-                            }
-                            event.location = location
-                            
-                            guard let updatedString = obj["updated_at"] as? String,
-                                let updatedAt = self.formatter.date(from: updatedString) as NSDate? else {
-                                    fatalError("ToDo: gracefully handle error")
-                            }
-                            event.updatedTime = updatedAt
-                            
-                            // okay, we haven't crashed by now so we guchi
-                            Schedule.sharedInstance.addEvent(event: event)
+                if let json = result as? [NSDictionary] {
+                    // loop through our valid json dictionary and create event objects that will be added to the schedule
+                    for obj in json {
+                        // create event objects
+                        let event = Event()
+                        
+                        guard let id = obj["id"] as? Int else {
+                            fatalError("ToDo: gracefully handle error")
                         }
+                        event.id = id
+                        
+                        guard let title = obj["title"] as? String else {
+                            fatalError("ToDo: gracefully handle error")
+                        }
+                        event.title = title
+                        
+                        guard let detail = obj["description"] as? String else {
+                            fatalError("ToDo: gracefully handle error")
+                        }
+                        event.detail = detail
+                        
+                        guard let timeStr = obj["time"] as? String,
+                            let time = self.formatter.date(from: timeStr) as NSDate? else {
+                            fatalError("ToDo: gracefully handle error")
+                        }
+                        event.time = time
+                        
+                        guard let location = obj["location"] as? String else {
+                            fatalError("ToDo: gracefully handle error")
+                        }
+                        event.location = location
+                        
+                        guard let updatedString = obj["updated_at"] as? String,
+                            let updatedAt = self.formatter.date(from: updatedString) as NSDate? else {
+                                fatalError("ToDo: gracefully handle error")
+                        }
+                        event.updatedTime = updatedAt
+                        
+                        // okay, we haven't crashed by now so we guchi
+                        Schedule.sharedInstance.addEvent(event: event)
                     }
-                    completionHandler(true)
                 }
+                completionHandler(true)
             }
         }
     }
